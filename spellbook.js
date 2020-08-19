2f8631701239e99b3552404c6b35183c1aaf98a9
Jason Teitelman <jasonmadesomething@gmail.com>
2020-08-18 20:21:46 -0400

    Refactor handler logic

--- spellbook.js
+++ spellbook.js
@@ -1,41 +1,28 @@
-// Run dotenv
 require('dotenv').config()
 const Discord = require('discord.js')
 const axios = require('axios')
 const Feats = require('./feats')
 const Magic = require('./magic')
+
 const client = new Discord.Client()
-// initialize bot by connecting to the server
 client.login(process.env.DISCORD_TOKEN)
-
-const baseUrl = 'https://www.d20pfsrd.com/'
 const responders = [new Magic(), new Feats()]
-// Bot event listener for a message
-client.on('message', handleMessage)
 
-async function handleMessage (msg) {
-  if (validateUrl(msg.content)) {
-    const siteUrl = encodeURI(msg.content)
-    const message = await getPage(siteUrl)
-    msg.channel.send(message)
-  }
-}
+client.on('message', getPage)
 
-async function getPage (url) {
+async function getPage (msg) {
+  const url = encodeURI(msg.content)
   const responder = responders.find(responder => responder.match(url))
-  try {
-    const response = await axios.get(url)
-    const message = responder.format(response)
-    return message
-  } catch (e) {
-    console.error(e)
-  }
-}
+  if (!responder) {
 
-function validateUrl (incomingMessage) {
-  if ((incomingMessage.startsWith(baseUrl + 'magic/') || incomingMessage.startsWith(baseUrl + 'feats/')) && !(incomingMessage.endsWith('feats/') && incomingMessage.endsWith('magic/'))) {
-    return true
-  } else {
-    return false
+  }
+  else {
+    try {
+      const response = await axios.get(url)
+      const message = responder.format(response)
+      msg.channel.send(message)
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
